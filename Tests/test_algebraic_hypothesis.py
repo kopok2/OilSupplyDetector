d87d15dd8017590c62407bd681980fe3668d27f9
Karol Oleszek <oleszek.karol@gmail.com>
2020-01-01 17:37:12 +0000

    Project structure

---
+++ Tests/test_algebraic_hypothesis.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+"""Test."""
+
+import os
+import sys
+import unittest
+
+TEST_DIR = os.path.dirname(__file__)
+SRC_DIR = '../Source'
+sys.path.insert(0, os.path.abspath(os.path.join(TEST_DIR, SRC_DIR)))
+
+import Module
+
+
+class TestSuite(unittest.TestCase):
+    def test_empty(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
