24debee26fe21282c7fe0690dc534ca31c55f911
Karol Oleszek <oleszek.karol@gmail.com>
2020-01-03 14:27:30 +0000

    Sat downloader

--- Source/sat_downloader.py
+++ Source/sat_downloader.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 
 LOC_FILE_PATH = 'locations.json'
 EXPLORER_URL = 'https://earthexplorer.usgs.gov/'
-
+SCREEN_DIR = 'screens'
 
 if __name__ == '__main__':
     # Load location list
@@ -33,10 +33,21 @@ if __name__ == '__main__':
 
         # zoom in
         num_zoom = 11
+        elem = driver.find_element_by_class_name('leaflet-control-zoom-in')
         for _ in range(num_zoom):
-            elem = driver.find_element_by_class_name('leaflet-control-zoom-in')
             elem.click()
             time.sleep(1)
-        while True:
-            pass
+
+        instructions = ['screen_cc'] + [Keys.ARROW_DOWN] * 9 + ['screen_cb'] + [Keys.ARROW_RIGHT] * 9 + ['screen_rb'] + \
+                       [Keys.ARROW_UP] * 9 + ['screen_rc'] + [Keys.ARROW_UP] * 9 + ['screen_ru'] + \
+                       [Keys.ARROW_LEFT] * 9 + \
+                       ['screen_cu'] + [Keys.ARROW_LEFT] * 9 + ['screen_lu'] + \
+                       [Keys.ARROW_DOWN] * 9 + ['screen_lc'] + [Keys.ARROW_DOWN] * 9 + ['screen_lb']
+        elem = driver.find_element_by_id('map')
+        for ins in instructions:
+            if 'screen' in ins:
+                driver.save_screenshot(SCREEN_DIR + '/' + loc['name'] + '_' + ins + '.jpg')
+            else:
+                elem.send_keys(ins)
+            time.sleep(1)
         driver.close()
