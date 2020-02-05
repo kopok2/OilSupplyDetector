9a11bbb0beb00ae0af87ed7f2cba8b461446142b
Karol Oleszek <oleszek.karol@gmail.com>
2020-02-05 21:19:17 +0000

    Screen scheduling

--- Source/crop_images.py
+++ Source/crop_images.py
@@ -10,17 +10,19 @@ IMG_HEIGHT = CROP_AREA[3] - CROP_AREA[1]
 SCREENS_DIR = "screens"
 CROPPED_DIR = "cropped"
 MERGED_DIR = "merged"
+X_SCREENS = 3
+Y_SCREENS = 3
 
 
 def merge_images(dir_path):
     """Merge images into bigger parts."""
-    new_im = Image.new('RGB', (3 * IMG_WIDTH, 3 * IMG_HEIGHT))
+    new_im = Image.new('RGB', (X_SCREENS * IMG_WIDTH, Y_SCREENS * IMG_HEIGHT))
 
-    new_im.save('test.jpg')
     for dir_name in os.listdir(dir_path):
         images = [(Image.open(dir_path + "/" + dir_name + "/" + img_f), img_f) for img_f in
                   os.listdir(dir_path + "/" + dir_name)]
-        ends = [end + ".jpg" for end in ['cb', 'cc', 'cu', 'lb', 'lc', 'lu', 'rb', 'rc', 'ru']]
+        #ends = [end + ".jpg" for end in ['cb', 'cc', 'cu', 'lb', 'lc', 'lu', 'rb', 'rc', 'ru']]
+        ends = [end + ".jpg" for end in [f"{x}_{y}" for x in range(X_SCREENS) for y in range(Y_SCREENS)]]
         img_paste = {}
         for end in ends:
             for image in images:
@@ -28,7 +30,7 @@ def merge_images(dir_path):
                     img_paste[end[:2]] = image[0]
 
         # Create merged image
-        new_im.paste(img_paste['lu'], (0, 0))
+        """new_im.paste(img_paste['lu'], (0, 0))
         new_im.paste(img_paste['lc'], (0, IMG_HEIGHT))
         new_im.paste(img_paste['lb'], (0, IMG_HEIGHT * 2))
         new_im.paste(img_paste['cu'], (IMG_WIDTH, 0))
@@ -36,7 +38,10 @@ def merge_images(dir_path):
         new_im.paste(img_paste['cb'], (IMG_WIDTH, IMG_HEIGHT * 2))
         new_im.paste(img_paste['ru'], (IMG_WIDTH * 2, 0))
         new_im.paste(img_paste['rc'], (IMG_WIDTH * 2, IMG_HEIGHT))
-        new_im.paste(img_paste['rb'], (IMG_WIDTH * 2, IMG_HEIGHT * 2))
+        new_im.paste(img_paste['rb'], (IMG_WIDTH * 2, IMG_HEIGHT * 2))"""
+        for x in range(X_SCREENS):
+            for y in range(Y_SCREENS):
+
 
         new_im.save(MERGED_DIR + "/" + dir_name + "_merged.jpg")
 
--- Source/sat_downloader.py
+++ Source/sat_downloader.py
@@ -9,6 +9,40 @@ from selenium.webdriver.common.keys import Keys
 LOC_FILE_PATH = 'locations.json'
 EXPLORER_URL = 'https://earthexplorer.usgs.gov/'
 SCREEN_DIR = 'screens'
+SCREEN_STEPS = 9
+X_SCREENS = 3
+Y_SCREENS = 3
+X_SCREENS_OFFSET = 1
+Y_SCREENS_OFFSET = 1
+
+
+def get_instructions(x_screens, y_screens, x_offset, y_offset, steps):
+    """Get instructions for screen scheduler.
+
+    Args:
+        x_screens: how many screens horizontally.
+        y_screens: how many screens vertically.
+        x_offset: how many screens of horizontal offset.
+        y_offset: how many screens of vertical offset.
+        steps: arrow key presses to pass screen.
+
+    Returns:
+        list of instructions for screens executor.
+    """
+    # Move to starting position
+    result = [Keys.ARROW_UP] * x_offset * steps + [Keys.ARROW_LEFT] * y_offset * steps
+
+    # First row
+    result += ["screen_0_0"]
+    for x in range(x_screens - 1):
+        result += [Keys.ARROW_RIGHT] * steps + [f"screen_{x + 1}_0"]
+    for row in range(y_screens - 1):
+        result += [Keys.ARROW_DOWN] * steps + [f"screen_{0 if row % 2 else x_screens - 1}_{row + 1}"]
+        for x in range(x_screens - 1):
+            result += [Keys.ARROW_RIGHT if row % 2 else Keys.ARROW_LEFT] * steps + \
+                      [f"screen_{x if row % 2 else x_screens - x}_{row + 1}"]
+    return result
+
 
 if __name__ == '__main__':
     # Load location list
@@ -55,6 +89,7 @@ if __name__ == '__main__':
                        [Keys.ARROW_LEFT] * 9 + \
                        ['screen_cu'] + [Keys.ARROW_LEFT] * 9 + ['screen_lu'] + \
                        [Keys.ARROW_DOWN] * 9 + ['screen_lc'] + [Keys.ARROW_DOWN] * 9 + ['screen_lb']
+        instructions = get_instructions(X_SCREENS, Y_SCREENS, X_SCREENS_OFFSET, Y_SCREENS_OFFSET, SCREEN_STEPS)
         elem = driver.find_element_by_id('map')
         for ins in instructions:
             if 'screen' in ins:
