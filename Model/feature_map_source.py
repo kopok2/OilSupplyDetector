5c9c1905c979f61522107bc4591e574ff758c0a2
Karol Oleszek <oleszek.karol@gmail.com>
2020-02-17 20:00:58 +0000

    Model

---
+++ Model/feature_map_source.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+"""Convolutional Neural Network image classifier for weight transfert into feature map creation."""
+
+import torch
---
+++ Model/localize.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""Localize bounding boxes of oil tanks.
+
+Algorithms used is SSD: Single Shot MultiBox Detector
+
+adapted from https://arxiv.org/pdf/1512.02325.pdf
+implemented in PyTorch
+"""
