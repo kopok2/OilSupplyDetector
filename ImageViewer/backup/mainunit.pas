d5434f4f22885aab797aa1848de44c1fd609fb7d
Karol Oleszek <oleszek.karol@gmail.com>
2020-03-01 16:51:56 +0000

    Image annotation.

--- ImageViewer/backup/mainunit.pas
+++ ImageViewer/backup/mainunit.pas
@@ -15,12 +15,21 @@ type
   { TMainForm }
 
   TMainForm = class(TForm)
+    SaveBtn: TButton;
     LoadButton: TButton;
     ControlPanel: TPanel;
     ImageDraw: TImage;
     ImagePanel: TPanel;
     ImageBox: TScrollBox;
+    DrawTimer: TTimer;
+    OpenDialog: TOpenDialog;
+    SaveDialog: TSaveDialog;
+    procedure DrawTimerTimer(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure ImageDrawMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
     procedure LoadButtonClick(Sender: TObject);
+    procedure SaveBtnClick(Sender: TObject);
   private
 
   public
@@ -29,6 +38,10 @@ type
 
 var
   MainForm: TMainForm;
+  Boxes: array[0..4096] of TRect;
+  BoxesCount: Integer;
+  Adding: Boolean;
+  AddingBox: TRect;
 
 implementation
 
@@ -38,7 +51,66 @@ implementation
 
 procedure TMainForm.LoadButtonClick(Sender: TObject);
 begin
-  ImageDraw.LoadFromFile(ImagePath + 'US Oil Indianapolis IN_screen__merged.jpg');
+  if OpenDialog.Execute then
+  begin
+       ImageDraw.Picture.LoadFromFile(OpenDialog.FileName);
+  end;
+end;
+
+procedure TMainForm.SaveBtnClick(Sender: TObject);
+var
+  OutFile: TStringList;
+  X: Integer;
+begin
+  if SaveDialog.Execute then
+  begin
+    OutFile:= TStringList.Create();
+    for X:= 0 to BoxesCount - 1 do begin
+      OutFile.Add(IntToStr(Boxes[X].Right) + ' ' +
+                  IntToStr(Boxes[X].Top) + ' ' +
+                  IntToStr(Boxes[X].Left) + ' ' +
+                  IntToStr(Boxes[X].Bottom));
+    end;
+    OutFile.SaveToFile(SaveDialog.FileName);
+  end;
+end;
+
+procedure TMainForm.ImageDrawMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+  if Adding then begin
+    Adding := False;
+    AddingBox.BottomRight := TPoint.Create(X, Y);
+    Boxes[BoxesCount] := AddingBox;
+    inc(BoxesCount);
+  end else begin
+    Adding := True;
+    AddingBox := TRect.Create(X, Y, 0, 0);
+  end;
+end;
+
+
+procedure TMainForm.DrawTimerTimer(Sender: TObject);
+var
+  X: Integer;
+begin
+  for X := 0 to BoxesCount - 1 do begin
+    with ImageDraw.Picture.Bitmap.Canvas do begin
+      Brush.Style:= BSClear;
+      Pen.Color:= clRED;
+      Pen.Width:= 2;
+      Font.Color:= clRed;
+      Font.Size:= 12;
+      Rectangle(Boxes[X]);
+      TextOut(Boxes[X].Left, Boxes[X].Bottom, 'Oil tank ' + IntToStr(X + 1));
+    end;
+  end;
+end;
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  BoxesCount:= 0;
+  Adding:= False;
 end;
 
 end.
